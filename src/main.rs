6de89ce65f1266249e6468c71ad2062759421df8
Martin GRUDLER <devel@grudler.eu>
2024-11-22 13:24:23 +0100

    Handle multiple connections using threads

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,44 @@
 #![allow(unused_imports)]
 
+use std::io;
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                loop {
-                    let packet_size = stream.read(&mut [0; 512]).unwrap();
-                    if packet_size == 0 {
-                        break;
-                    }
-
-                    stream.write_all(b"+PONG\r\n").unwrap();
-                }
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+async fn process_socket(mut socket: TcpStream) {
+    loop {
+        let packet_size = socket.read(&mut [0; 512]).await.expect("Failed to read stream.");
+        if packet_size == 0 {
+            break;
         }
+        socket.write_all(b"+PONG\r\n").await.expect("Failed to write stream.");
     }
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
+
+    loop {
+        let (socket, _) = listener.accept().await?;
+        process_socket(socket).await;
+    }
+
+    // for stream in listener.incoming() {
+    //     match stream {
+    //         Ok(mut stream) => {
+    //             loop {
+    //                 let packet_size = stream.read(&mut [0; 512]).unwrap();
+    //                 if packet_size == 0 {
+    //                     break;
+    //                 }
+    //
+    //                 stream.write_all(b"+PONG\r\n").unwrap();
+    //             }
+    //         }
+    //         Err(e) => {
+    //             println!("error: {}", e);
+    //         }
+    //     }
+    // }
 }
